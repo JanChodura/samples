982448358af94272255b3c1fe3d3a7c182cfb2bb
Jan Chodura <jnchodura@gmail.com>
2016-02-01 19:00:23 +0100

    +: finish of ArrayQueue

--- Algorithms/src/main/java/cz/chodura/algoritmus/queue/ArrayQueue.java
+++ Algorithms/src/main/java/cz/chodura/algoritmus/queue/ArrayQueue.java
@@ -1,75 +1,78 @@
 package cz.chodura.algoritmus.queue;
 
 public class ArrayQueue<E> {
-    
-    private static int DEFAULT_CAPACITY = 10;
-    
-    private E[] queue;
-    
-    private int rear;
-    
-    private int front;
-    
-    private int N;
-    
-    
-    public ArrayQueue() {
-    
-        this(DEFAULT_CAPACITY);
-    }
-    
-    
-    public ArrayQueue(int capacity) {
-    
-        queue = (E[]) new Object[capacity];
-        rear = front = -1;
-        N = capacity;
-    }
-    
-    
-    public void enqueue(E object) {
-    
-        if (isFull()) {
-            return;
-        }
-        
-        if (isEmpty()) {
-            
-            front++;
-            rear++;
-        } else {
-            rear++;
-        }
-        
-        queue[rear] = object;
-        
-    }
-    
-    
-    public E dequeue() {
-    
-        if (isEmpty()) {
-            return null;
-        }
-        
-        if (front == rear) {
-            front = rear = -1;
-        } else {
-            front++;
-        }
-        
-        return queue[front];
-    }
-    
-    
-    public boolean isEmpty() {
-    
-        return rear == -1 && front == -1;
-    }
-    
-    
-    public boolean isFull() {
-    
-        return front == N - 1;
-    }
+
+   private static int DEFAULT_CAPACITY = 10;
+
+   private E[] queue;
+
+   private int rear;
+
+   private int front;
+
+   private int N;
+
+   public ArrayQueue() {
+
+      this(DEFAULT_CAPACITY);
+   }
+
+   public ArrayQueue(int capacity) {
+
+      queue = (E[]) new Object[capacity];
+      rear = front = -1;
+      N = capacity;
+   }
+
+   /**
+    * 
+    * @author Jan Chodura
+    * @created 1. 2. 2016
+    *
+    * @param object if is Queue full nothing is filled.
+    */
+   public void enqueue(E object) {
+
+      if (isFull()) {
+         return;
+      }
+
+      if (isEmpty()) {
+
+         front++;
+      }
+      rear = (rear + 1) % N;
+
+      queue[rear] = object;
+
+   }
+
+   public void dequeue() {
+
+      if (isEmpty()) {
+         return;
+      }
+
+      if (front == rear) {
+         front = rear = -1;
+      }
+      else {
+         front = (front + 1) % N;
+      }
+   }
+
+   public E front() {
+
+      return queue[front];
+   }
+
+   public boolean isEmpty() {
+
+      return rear == -1 && front == -1;
+   }
+
+   public boolean isFull() {
+
+      return front == (rear + 1) % N;
+   }
 }
--- Algorithms/src/test/java/cz/chodura/algoritmus/queue/ArrayQueueTest.java
+++ Algorithms/src/test/java/cz/chodura/algoritmus/queue/ArrayQueueTest.java
@@ -1,23 +1,59 @@
 package cz.chodura.algoritmus.queue;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-
 public class ArrayQueueTest {
-    
-    @Test
-    public void testEnqueue()
-            throws Exception {
-        
-        ArrayQueue<String> queue = new ArrayQueue<String>(6);
-        
-        queue.enqueue("John");
-        
-        assertEquals("John", queue.dequeue());
-        assertTrue(queue.isEmpty());
-    }
-    
+
+   @Test
+   public void testEnqueue() throws Exception {
+
+      // Arrange
+      ArrayQueue<String> queue = new ArrayQueue<String>(6);
+
+      // Act
+      queue.enqueue("John");
+
+      // Assert
+      assertEquals("John", queue.front());
+   }
+
+   @Test
+   public void testDequeue() throws Exception {
+
+      // Arrange
+      ArrayQueue<String> queue = new ArrayQueue<String>(6);
+      queue.enqueue("John");
+      queue.enqueue("Susan");
+
+      // Act
+      queue.dequeue();
+      String onlyOneName = queue.front();
+      queue.dequeue();
+
+      // Assert
+      assertEquals("Susan", onlyOneName);
+      assertTrue(queue.isEmpty());
+   }
+
+   @Test
+   public void testRotation() throws Exception {
+
+      // Arrange
+      ArrayQueue<String> queue = new ArrayQueue<String>(3);
+      queue.enqueue("John");
+      queue.enqueue("Susan");
+      queue.enqueue("Peggy");
+
+      // Act
+      queue.dequeue();
+      queue.enqueue("Rogger");
+
+      // Assert
+      assertEquals("Susan", queue.front());
+      assertTrue(queue.isFull());
+   }
+
 }
