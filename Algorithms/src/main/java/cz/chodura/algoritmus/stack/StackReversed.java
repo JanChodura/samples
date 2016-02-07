579160416a237c5142ed9df9d4447f124037cf70
Jan Chodura <jnchodura@gmail.com>
2016-02-07 09:41:44 +0100

    .

--- Algorithms/src/main/java/cz/chodura/algoritmus/stack/StackReversed.java
+++ Algorithms/src/main/java/cz/chodura/algoritmus/stack/StackReversed.java
@@ -1,15 +1,9 @@
 package cz.chodura.algoritmus.stack;
 
-/**
- * Implemenation of reversed functions for Stack.
- * 
- * @author jach
- *
- */
 public class StackReversed {
-    
-    public String reversedString(String input){
-        
-        return null;
-    }
+
+   public String reversedString(String input) {
+
+      return null;
+   }
 }
